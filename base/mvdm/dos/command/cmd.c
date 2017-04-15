e91feb9b6e49e2f4c185516b9bc1a81b92f13932
Stephanos Ioannidis <root@stephanos.io>
2017-04-15 19:29:14 +0900

    command updated to NT 5.2 version.

--- base/mvdm/dos/command/cmd.c
+++ base/mvdm/dos/command/cmd.c
@@ -6,29 +6,19 @@
 
 #include "cmd.h"
 #include "cmdsvc.h"
+#include "host_def.h"
 
 
-/* CmdInit - COmmand Initialiazation routine.
+/* CmdInit - Command Initialiazation routine.
  *
  * Entry
- *	argc,argv - from softpc as it is.
- *		    Full path name of the dos binary is preceded with
- *		    -a or /a. i.e. -a c:\nt\bin86\kernel.exe reversi.exe
- *
- *
+ *          None
  * Exit
+ *          None
  *
  */
 
-BOOL CMDInit (argc,argv)
-INT	argc;
-PSZ	*argv;
+VOID CMDInit (VOID)
 {
-CHAR  RootDir [MAX_PATH];
-UINT  Len;
-
-    Len = GetSystemDirectory (RootDir,MAX_PATH);
-    if (Len <= MAX_PATH && Len > 0)
-	cmdHomeDirectory[0] = RootDir[0];
-    return TRUE;
+	cmdHomeDirectory[0] = *pszSystem32Path;
 }
